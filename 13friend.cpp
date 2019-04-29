e824228564093f8fb4b9b4bd2fef95e88dfc6da2
Dan Li <akiraryo@hotmail.com>
2019-04-29 21:49:36 +0200

    构造完成

--- 13friend.cpp
+++ 13friend.cpp
@@ -1,15 +1,23 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
 class Constractor
 {
+public://构造函数一;
+	Constractor() :m_real(0),m_image(0){}
+public://构造函数二;
+	Constractor(int real, int image)
+	{
+		m_real = real;
+		m_image = image;
+	}
 public:
-	Constractor(int d = 0) :data(d){}
 
-public:
+
 
 private:
-	int data;
+	int m_real;
+	int m_image;
 };
 
 int main()
