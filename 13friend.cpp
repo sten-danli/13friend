12a57912586179b0c01e47a3861661074aec198a
Dan Li <akiraryo@hotmail.com>
2019-04-29 22:52:41 +0200

    声明友元函数

--- 13friend.cpp
+++ 13friend.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 class Constractor
 {
+	//声明友元关系；
+friend Constractor operator+(int i, const Constractor& c);
+
 public://构造函数一;
 	Constractor() :m_real(0),m_image(0){}
 public://构造函数二;
@@ -23,16 +26,21 @@ public://func加法;
 		return Constractor(m_real + c.m_real, m_image + c.m_image);
 	}
 
-public:
+public://构造函数加法；
 	Constractor operator+(Constractor& c)
 	{
 		return Constractor(m_real + c.m_real, m_image + c.m_image);
 	}
+
 private:
 	int m_real;
 	int m_image;
 };
-
+//全局函数；
+Constractor operator+(int i, const Constractor& c)
+{
+	return Constractor(c.m_real + i, c.m_image);
+}
 int main()
 {
 	Constractor x;
