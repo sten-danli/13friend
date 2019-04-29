1afa894c0779305ff7a99a13a63ed2f4eab7db15
Dan Li <akiraryo@hotmail.com>
2019-04-29 21:44:18 +0200

    构造函数完成

---
+++ 13friend.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Constractor
+{
+public:
+	Constractor(int d = 0) :data(d){}
+
+public:
+
+private:
+	int data;
+};
+
+int main()
+{
+	cout << "Hello World" << endl;
+}
